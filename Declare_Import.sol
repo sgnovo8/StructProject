19d991b2b3cb30a380dc6a99cf58f8e25144ff2c
sgnovo8 <88598099+sgnovo8@users.noreply.github.com>
2022-01-05 10:42:36 -0500

    Create Declare_Import.sol

---
+++ Declare_Import.sol
@@ -0,0 +1,24 @@
+// Declaring and importing Struct
+// File that the struct is declared in:
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+struct Todo {
+    string text;
+    bool completed;
+}
+
+// File that imports the struct above
+
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import "./StructDeclaration.sol";
+
+contract Todos {
+    // An array of 'Todo' structs
+    Todo[] public todos;
+}
+
